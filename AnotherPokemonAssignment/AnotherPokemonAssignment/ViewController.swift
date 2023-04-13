bcf813cd7111a337b27c79b0f2a68882f5061f2a
Mark Chen <lazzyjessie@gmail.com>
2023-04-13 11:20:51 +0800

    [Chore] init project

---
+++ AnotherPokemonAssignment/AnotherPokemonAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnotherPokemonAssignment
+//
+//  Created by Mark Chen on 2023/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnotherPokemonAssignment/AnotherPokemonAssignmentUITests/AnotherPokemonAssignmentUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnotherPokemonAssignmentUITestsLaunchTests.swift
+//  AnotherPokemonAssignmentUITests
+//
+//  Created by Mark Chen on 2023/4/13.
+//
+
+import XCTest
+
+final class AnotherPokemonAssignmentUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
