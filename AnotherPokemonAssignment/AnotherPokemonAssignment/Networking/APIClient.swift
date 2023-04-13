dee56bd7d81d5225ec7c41fcb2be550b393ad409
Mark Chen <lazzyjessie@gmail.com>
2023-04-13 13:17:12 +0800

    [Chore] add `APIClient` file

---
+++ AnotherPokemonAssignment/AnotherPokemonAssignment/Networking/APIClient.swift
@@ -0,0 +1,12 @@
+//
+//  APIClient.swift
+//  AnotherPokemonAssignment
+//
+//  Created by Mark Chen on 2023/4/13.
+//
+
+import Foundation
+
+final class APIClient {
+    private let session = URLSession.shared
+}
