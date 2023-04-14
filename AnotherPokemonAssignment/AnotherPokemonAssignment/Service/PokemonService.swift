58ccb724501f7d9a08d282303f085dc0fc3f74cd
Mark Chen <lazzyjessie@gmail.com>
2023-04-14 11:29:04 +0800

    [Feat] add `PokemonService`

---
+++ AnotherPokemonAssignment/AnotherPokemonAssignment/Service/PokemonService.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonService.swift
+//  AnotherPokemonAssignment
+//
+//  Created by Mark Chen on 2023/4/14.
+//
+
+import Foundation
+
+final class PokemonService {
+    private let decoder = JSONDecoder()
+}
